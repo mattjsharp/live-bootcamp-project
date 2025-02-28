e198ee44fe041b2922e7bbe62c3ee40297adc75b
mattjsharp <matthewsharp@live.com>
2025-02-28 14:16:34 -0600

    [Sprint 5]Added postgres userstore

--- auth-service/src/main.rs
+++ auth-service/src/main.rs
@@ -1,12 +1,13 @@
 use std::sync::Arc;
 
-use auth_service::{app_state::AppState, get_postgres_pool, services::{data_stores::{hashmap_two_fa_code_store::HashmapTwoFACodeStore, hashmap_user_store::HashmapUserStore, hashset_banned_token_store::HashsetBannedTokenStore}, mock_email_client::MockEmailClient}, utils::constants::{prod, DATABASE_URL}, Application};
+use auth_service::{app_state::AppState, get_postgres_pool, services::{data_stores::{hashmap_two_fa_code_store::HashmapTwoFACodeStore, hashmap_user_store::HashmapUserStore, hashset_banned_token_store::HashsetBannedTokenStore, postgres_user_store::PostgresUserStore}, mock_email_client::MockEmailClient}, utils::constants::{prod, DATABASE_URL}, Application};
 use sqlx::PgPool;
 use tokio::sync::RwLock;
 
 #[tokio::main]
 async fn main() {
-    let user_store = Arc::new(RwLock::new(HashmapUserStore::default()));
+    let pg_pool = configure_postgresql().await;
+    let user_store = Arc::new(RwLock::new(PostgresUserStore::new(pg_pool)));
     let banned_token_store = Arc::new(RwLock::new(HashsetBannedTokenStore::default()));
     let two_fa_code_store = Arc::new(RwLock::new(HashmapTwoFACodeStore::default()));
     let email_client = Arc::new(RwLock::new(MockEmailClient));
--- auth-service/src/services/data_stores/postgres_user_store.rs
+++ auth-service/src/services/data_stores/postgres_user_store.rs
@@ -26,24 +26,43 @@ impl PostgresUserStore {
 impl UserStore for PostgresUserStore {
     async fn add_user(&mut self, user: User) -> Result<(), UserStoreError> {
 
-        // sqlx::query!(
-        //     r#"
-        //     INSERT INTO users (email, password_hash, requires_2fa)
-        //     VALUES ($1, $2, $3)
-        //     "#,
-        //     user.email.as_ref(),
-        //     user.password.as_ref(),
-        //     user.requires_2fa
-        // )
-        // .execute(&self.pool)
-        // .await
-        // .map_err(|_| UserStoreError::UnexpectedError)?;
+        sqlx::query!(
+            r#"
+            INSERT INTO users (email, password_hash, requires_2fa)
+            VALUES ($1, $2, $3)
+            "#,
+            user.email.as_ref(),
+            user.password.as_ref(),
+            user.requires_2fa
+        )
+        .execute(&self.pool)
+        .await
+        .map_err(|_| UserStoreError::UnexpectedError)?;
 
         Ok(())
     }
     async fn get_user(&self, email: &Email) -> Result<User, UserStoreError> {
        
-       todo!()
+        sqlx::query!(
+            r#"
+            SELECT email, password_hash, requires_2fa
+            FROM users
+            WHERE email = $1
+            "#,
+            email.as_ref()
+        )
+        .fetch_optional(&self.pool)
+        .await
+        .map_err(|_| UserStoreError::UnexpectedError)?
+        .map(|row| {
+            Ok(User {
+                email: Email::parse(row.email.as_ref()).map_err(|_| UserStoreError::UnexpectedError)?,
+                password: Password::parse(row.password_hash.as_ref())
+                    .map_err(|_| UserStoreError::UnexpectedError)?,
+                requires_2fa: row.requires_2fa,
+            })
+        })
+        .ok_or(UserStoreError::UserNotFound)?
     }
 
     async fn validate_user(
@@ -51,44 +70,44 @@ impl UserStore for PostgresUserStore {
         email: &Email,
         password: &Password,
     ) -> Result<(), UserStoreError> {
-        // let user = self.get_user(email).await?;
-        // if user.password == *password {
-        //     return Ok(());
-        // }
+        let user = self.get_user(email).await?;
+        if user.password == *password {
+            return Ok(());
+        }
         Err(UserStoreError::InvalidCredentials)
     }
 }
 
-// Helper function to verify if a given password matches an expected hash
-// TODO: Hashing is a CPU-intensive operation. To avoid blocking
-// other async tasks, update this function to perform hashing on a
-// separate thread pool using tokio::task::spawn_blocking. Note that you
-// will need to update the input parameters to be String types instead of &str
-fn verify_password_hash(
-    expected_password_hash: &str,
-    password_candidate: &str,
-) -> Result<(), Box<dyn Error>> {
-    let expected_password_hash: PasswordHash<'_> = PasswordHash::new(expected_password_hash)?;
+async fn verify_password_hash(
+    expected_password_hash: String,
+    password_candidate: String,
+) -> Result<(), Box<dyn Error + Send + Sync>> {
+    let result = tokio::task::spawn_blocking(move || {
+        let expected_password_hash: PasswordHash<'_> = PasswordHash::new(&expected_password_hash)?;
 
-    Argon2::default()
-        .verify_password(password_candidate.as_bytes(), &expected_password_hash)
-        .map_err(|e| e.into())
+        Argon2::default()
+            .verify_password(password_candidate.as_bytes(), &expected_password_hash)
+            .map_err(|e| e.into())
+    })
+    .await;
+
+    result?
 }
 
-// Helper function to hash passwords before persisting them in the database.
-// TODO: Hashing is a CPU-intensive operation. To avoid blocking
-// other async tasks, update this function to perform hashing on a
-// separate thread pool using tokio::task::spawn_blocking. Note that you
-// will need to update the input parameters to be String types instead of &str
-fn compute_password_hash(password: &str) -> Result<String, Box<dyn Error>> {
-    let salt: SaltString = SaltString::generate(&mut rand::thread_rng());
-    let password_hash = Argon2::new(
-        Algorithm::Argon2id,
-        Version::V0x13,
-        Params::new(15000, 2, 1, None)?,
-    )
-    .hash_password(password.as_bytes(), &salt)?
-    .to_string();
+async fn compute_password_hash(password: String) -> Result<String, Box<dyn Error + Send + Sync>> {
+    let result = tokio::task::spawn_blocking(move || {
+        let salt: SaltString = SaltString::generate(&mut rand::thread_rng());
+        let password_hash = Argon2::new(
+            Algorithm::Argon2id,
+            Version::V0x13,
+            Params::new(15000, 2, 1, None)?,
+        )
+        .hash_password(password.as_bytes(), &salt)?
+        .to_string();
 
-    Ok(password_hash)
+        Ok(password_hash)
+    })
+    .await;
+
+    result?
 }
